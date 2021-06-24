f56b5f74aee5154c0f0342576087c2efe2ada467
anirudhbelwadi <anirudh.belwadi@gmail.com>
2021-06-24 12:50:56 +0400

    offers section rectified

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,30 +1,38 @@
-const no_of_carousel_items = $(".carousel_item").length;
-if (no_of_carousel_items == 1) {
-    $('#offers_carousel_left_button').hide();
-    $('#offers_carousel_right_button').hide();
+// stores no of offers added
+const no_of_offers = $(".offer_item").length;
+if (no_of_offers == 1) {
+    $('#offers_control_left_button').hide();
+    $('#offers_control_right_button').hide();
 }
-var active_carousel_item = 0;
+else{
+    $('#offers_control_left_button').show();
+    $('#offers_control_right_button').show();
+    for(i=1;i<no_of_offers;i++){
+        document.getElementsByClassName('offer_item')[i].style.display = 'none';
+    }
+}
+var active_offer = 0;
 
-$('#offers_carousel_left_button').on('click', () => {
-    document.getElementsByClassName('carousel_item')[active_carousel_item].style.display = 'none';
-    if (active_carousel_item == 0) {
-        document.getElementsByClassName('carousel_item')[no_of_carousel_items - 1].style.display = 'inherit';
-        active_carousel_item = no_of_carousel_items - 1
+$('#offers_control_left_button').on('click', () => {
+    document.getElementsByClassName('offer_item')[active_offer].style.display = 'none';
+    if (active_offer == 0) {
+        document.getElementsByClassName('offer_item')[no_of_offers - 1].style.display = 'inherit';
+        active_offer = no_of_offers - 1
     }
     else {
-        document.getElementsByClassName('carousel_item')[active_carousel_item - 1].style.display = 'inherit';
-        active_carousel_item = active_carousel_item - 1
+        document.getElementsByClassName('offer_item')[active_offer - 1].style.display = 'inherit';
+        active_offer = active_offer - 1
     }
 });
-$('#offers_carousel_right_button').on('click', () => {
-    document.getElementsByClassName('carousel_item')[active_carousel_item].style.display = 'none';
-    if (active_carousel_item == no_of_carousel_items - 1) {
-        document.getElementsByClassName('carousel_item')[0].style.display = 'inherit';
-        active_carousel_item = 0
+$('#offers_control_right_button').on('click', () => {
+    document.getElementsByClassName('offer_item')[active_offer].style.display = 'none';
+    if (active_offer == no_of_offers - 1) {
+        document.getElementsByClassName('offer_item')[0].style.display = 'inherit';
+        active_offer = 0
     }
     else {
-        document.getElementsByClassName('carousel_item')[active_carousel_item + 1].style.display = 'inherit';
-        active_carousel_item = active_carousel_item + 1
+        document.getElementsByClassName('offer_item')[active_offer + 1].style.display = 'inherit';
+        active_offer = active_offer + 1
     }
 });
 
